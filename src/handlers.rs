6e3e0d26ad09edc3a25f32ed0820c4ed848be4cc
UnRealReincarlution <benwhite2413@gmail.com>
2022-07-13 09:19:25 +1200

    Trying to remove deadlock2

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,4 +1,5 @@
 
+use std::clone;
 use std::time::{SystemTime, Duration};
 use std::{convert::Infallible};
 use reqwest::{Client};
@@ -28,19 +29,18 @@ pub async fn register_server(
 
     let temp_instance_stack = &configuration.lock().await.instance_stack;
     let mut locked_stack = temp_instance_stack.lock().await;
-    let exists_node = locked_stack.get_mut(&ip).cloned();
-    let cloned_node = exists_node.clone();
+    let exists_node = locked_stack.get_mut(&ip);
 
     drop(temp_instance_stack);
-    drop(exists_node);
-    drop(locked_stack);
 
-    let node = match cloned_node {
+    let node = match exists_node {
         Some(node) => {
             node.clone()
         },
         None => {
             println!("No node currently exists, creating and registering a new node.");
+            drop(exists_node);
+            drop(locked_stack);
 
             let client = &configuration.lock().await.client;
 
